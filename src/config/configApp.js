12c8e54d447f4ed5dcf2fd2747e89fd76cd542af
KSFirstUser <rusakk@mail.ru>
2019-11-24 02:37:15 +0300

    first try

---
+++ src/config/configApp.js
@@ -0,0 +1,4 @@
+export const configApp = {
+  apiKey4: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NGIzMWVhYThlMmVmZDllYWVhOWUwOGE4M2NiNTVkMyIsInN1YiI6IjVkZDk5MzU1MTJhYWJjMDAxMTkxNzc4ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.YiCVUv7A344kmepRE9bQTk1uG0t_-25VTQJtVmxc-9k'
+}
+// # sourceMappingURL=configApp.js.map
